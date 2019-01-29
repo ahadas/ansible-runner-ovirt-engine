47badbc302e89bb81da7c361bd8f0bda2b956c6f
Arik Hadas <arik.hadas1@gmail.com>
2019-01-29 13:24:30 +0100

    basing on ansible/ansible-runner rather than creating our own image

--- hack/run.sh
+++ hack/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec docker run --rm -v $(pwd)/playbooks/:/root:z  -v $(pwd)/env/settings:/runner/env/settings:z -e RUNNER_PLAYBOOK=/root/test.yml ansible-runner-engine
+exec docker run --rm -v $(pwd)/ansible-runner-ovirt-engine:/usr/lib/python2.7/site-packages/ansible-runner-ovirt-engine:z -v $(pwd)/playbooks/:/root:z  -v $(pwd)/env/settings:/runner/env/settings:z -e RUNNER_PLAYBOOK=/root/test.yml ansible/ansible-runner
